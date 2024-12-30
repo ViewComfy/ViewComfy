8e7aab482d82f67eb38eae0139592552aac29071
Jean Delannoy <jjdelannoy@hotmail.com>
2024-12-30 17:28:16 -0300

    removed dead code

--- pages/view-comfy/view-comfy-form-editor.tsx
+++ pages/view-comfy/view-comfy-form-editor.tsx
@@ -118,14 +118,6 @@ export function ViewComfyFormEditor({ onSubmit, viewComfyJSON }: ViewComfyFormEd
                 editMode={true}
                 downloadViewComfyJSON={downloadViewComfyJSON}
                 >
-                {/* <div className={cn("sticky bottom-0 p-4 bg-background w-full flex flex-row gap-x-4 rounded-md")}>
-                    <Button type="submit" className="w-full mb-2">
-                        Save Changes
-                    </Button>
-                    <Button variant="secondary" className="w-full" onClick={form.handleSubmit(downloadViewComfyJSON)}>
-                        Download as ViewComfy JSON
-                    </Button>
-                </div> */}
             </ViewComfyForm>
         </div>
     )
