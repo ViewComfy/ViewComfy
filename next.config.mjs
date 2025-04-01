53151a15224a2463d75e1acdc2d009759493efb1
Jean Delannoy <jjdelannoy@hotmail.com>
2025-03-31 21:32:17 -0300

    added the phase config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,29 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: { remotePatterns: [{ hostname: "*" }] },
-  output: "standalone",
-  // basePath: "/viewcomfy",
-  assetPrefix: "/viewcomfy",
-};
+// import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
 
-export default nextConfig;
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: { remotePatterns: [{ hostname: "*" }] },
+//   output: "standalone",
+//   // basePath: "/viewcomfy",
+//   assetPrefix: "/viewcomfy",
+// };
+
+// export default nextConfig;
+
+
+// @ts-check
+import { PHASE_DEVELOPMENT_SERVER } from "next/constants.js";
+
+export default (phase) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    images: { remotePatterns: [{ hostname: "*" }] },
+    output: "standalone",
+    assetPrefix: isDev ? undefined : "/viewcomfy",
+    basePath: isDev ? undefined : "/viewcomfy",
+  }
+  return nextConfig
+}
